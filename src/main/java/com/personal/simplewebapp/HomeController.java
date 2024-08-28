2a8f5807a362301e4b17cdfbc141a3f26141fd76
Aman Varshney <amanvarshney212@gmail.com>
2024-08-28 17:51:37 +0530

    Implemented simple controllers. Learned three layers, Controller, Service and Repository as well as MVC architecture.

---
+++ src/main/java/com/personal/simplewebapp/HomeController.java
@@ -0,0 +1,18 @@
+package com.personal.simplewebapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet() {
+        return "Welcome to Simple Web App!";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "This is Aman. I am a passionate Software Engineer.";
+    }
+}
