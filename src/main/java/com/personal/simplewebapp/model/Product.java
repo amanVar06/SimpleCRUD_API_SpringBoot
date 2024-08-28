0f8aefcb58e8fc9b36b905754ea325148ef147d6
Aman Varshney <amanvarshney212@gmail.com>
2024-08-28 20:56:55 +0530

    Removed unused import.

--- src/main/java/com/personal/simplewebapp/model/Product.java
+++ src/main/java/com/personal/simplewebapp/model/Product.java
@@ -2,7 +2,6 @@ package com.personal.simplewebapp.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.ToString;
 
