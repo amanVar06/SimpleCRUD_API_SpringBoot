63366583b455af9e4e6315419ff441776a133bf9
Aman Varshney <amanvarshney212@gmail.com>
2024-08-28 18:45:10 +0530

    Removed unused import statement

--- src/main/java/com/personal/simplewebapp/controller/ProductController.java
+++ src/main/java/com/personal/simplewebapp/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.personal.simplewebapp.controller;
 import com.personal.simplewebapp.model.Product;
 import com.personal.simplewebapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
